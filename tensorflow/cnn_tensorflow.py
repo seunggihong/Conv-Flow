7fcc60a4d08b535843d1274056f08855a3d29e0a
onggi <hsk1533@naver.com>
2023-11-06 23:43:28 +0900

    branch test

--- tensorflow/cnn_tensorflow.py
+++ tensorflow/cnn_tensorflow.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.keras import models, layers
 import mlflow
 
+# test mac branch
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
