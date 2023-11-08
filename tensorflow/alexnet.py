cbd9942812602138e36b6bbb8684233edb98e319
onggi <hsk1533@naver.com>
2023-11-08 13:21:59 +0900

    alexnet cifar10 test

--- tensorflow/alexnet.py
+++ tensorflow/alexnet.py
@@ -2,43 +2,47 @@ from tensorflow.keras import layers, models, datasets
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-# load CIFAR-10 dataset
-# cifar10 = datasets.cifar10
-# (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+cifar10 = datasets.cifar10
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-# dispersion learning settings
-strategy = tf.distribute.MirroredStrategy()
+y_train = tf.keras.utils.to_categorical(y_train, 10)
+y_test = tf.keras.utils.to_categorical(y_test, 10)
 
-# data input pipeline
+Alexnet = models.Sequential(
+    [
+        layers.experimental.preprocessing.Resizing(
+            227, 227, interpolation='bilinear', input_shape=x_train.shape[1:]),
 
+        layers.Conv2D(filters=96, kernel_size=(11, 11), strides=(
+            4, 4), input_shape=(224, 224, 3), activation='relu'),
+        layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+        layers.BatchNormalization(),
 
-# Alexnet model
-with strategy.scope():
-    model = models.Sequential(
-        [
-            layers.Conv2D(filters=48, kernel_size=(11, 11),
-                          strides=(4, 4), input_shape=(224, 224, 3), activation='relu'),
-            layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+        layers.Conv2D(filters=256, kernel_size=(5, 5), strides=(
+            1, 1), activation='relu', padding='same'),
+        layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+        layers.BatchNormalization(),
 
-            layers.Conv2D(filters=128, kernel_size=(5, 5),
-                          strides=(1, 1), activation='relu', padding='same'),
-            layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+        layers.Conv2D(filters=384, kernel_size=(3, 3), strides=(
+            1, 1), activation='relu', padding='same'),
+        layers.Conv2D(filters=384, kernel_size=(3, 3), strides=(
+            1, 1), activation='relu', padding='same'),
+        layers.Conv2D(filters=256, kernel_size=(3, 3), strides=(
+            1, 1), activation='relu', padding='same'),
+        layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+        layers.BatchNormalization(),
 
-            layers.Conv2D(filters=192, kernel_size=(3, 3),
-                          strides=(1, 1), activation='relu', padding='same'),
-            layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+        layers.Flatten(),
+        layers.Dense(4096, activation='relu'),
+        layers.Dropout(.5),
+        layers.Dense(4096, activation='relu'),
+        layers.Dropout(.5),
+        layers.Dense(10, activation='softmax')
+    ]
+)
 
-            layers.Conv2D(filters=192, kernel_size=(3, 3),
-                          strides=(1, 1), activation='relu', padding='same'),
-            layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+Alexnet.compile(optimizer=tf.optimizers.SGD(learning_rate=0.001),
+                loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+Alexnet.summary()
 
-            layers.Conv2D(filters=128, kernel_size=(3, 3),
-                          strides=(1, 1), activation='relu', padding='same'),
-            layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
-
-            layers.Flatten(),
-            layers.Dense(2048, activation='relu'),
-            layers.Dense(2048, activation='relu'),
-            layers.Dense(10, activation='softmax')
-        ]
-    )
+Alexnet.fit(x_train, y_train, epochs=1)
