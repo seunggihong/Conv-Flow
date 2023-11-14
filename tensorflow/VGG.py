e8318fa6187b667081a7ff354ad64de9714dc4d3
onggi <hsk1533@naver.com>
2023-11-14 10:54:36 +0900

    vgg error fix

--- tensorflow/VGG.py
+++ tensorflow/VGG.py
@@ -1,5 +1,6 @@
+from statistics import mode
 import tensorflow as tf
-from tensorflow.keras import layers, Sequential, Model
+from tensorflow.keras import layers, models
 
 cfg = {
     'A': [64,     'M', 128,      'M', 256, 256,           'M', 512, 512,           'M', 512, 512,           'M'],
@@ -9,59 +10,26 @@ cfg = {
 }
 
 
-class VGG(Model):
-    def __init__(self, features, num_classes, input_shape=(32, 32, 3)):
-        super(VGG, self).__init__()
-
-        self.features = Sequential([
-            layers.Input(input_shape),
-            features
-        ])
-
-        self.classifier = Sequential([
-            layers.Dense(4096, activation='relu'),
-            layers.Dropout(0.5),
-            layers.Dense(4096, activation='relu'),
-            layers.Dropout(0.5),
-            layers.Dense(num_classes, activation='softmax'),
-        ])
-
-    def call(self, inputs, training=False):
-        x = self.features(inputs, training=training)
-        x = self.classifier(x, training=training)
-        return x
-
-
-def make_layers(cfg):
-    nets = []
-
-    for l in cfg:
-        if l == 'M':
-            nets += [layers.MaxPool2D()]
+def create_layers(config, num_classes, input_shape=(32, 32, 3)):
+    model = models.Sequential()
+    for i in config:
+        if i == "M":
+            model.add(layers.MaxPool2D())
             continue
+        model.add(layers.Conv2D(i, kernel_size=(3, 3),
+                  input_shape=input_shape, padding='same'))
+        model.add(layers.BatchNormalization())
+        model.add(layers.ReLU())
 
-        nets += [layers.Conv2D(l, (3, 3), padding='same')]
-        nets += [layers.BatchNormalization()]
-        nets += [layers.ReLU()]
-    return Sequential(nets)
+    model.add(layers.Dense(4096, activation='relu'))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(4096, activation='relu'))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(num_classes, activation='softmax'))
 
-
-def VGG11(num_classes):
-    return VGG(make_layers(cfg['A']), num_classes)
-
-
-def VGG13(num_classes):
-    return VGG(make_layers(cfg['B']), num_classes)
-
-
-def VGG16(num_classes):
-    return VGG(make_layers(cfg['D']), num_classes)
-
-
-def VGG19(num_classes):
-    return VGG(make_layers(cfg['E']), num_classes)
+    return model
 
 
 if __name__ == "__main__":
-    vgg11 = VGG11(10)
-    vgg11.summary()
+    vgg_A = create_layers(cfg['A'], 10)
+    vgg_A.summary()
